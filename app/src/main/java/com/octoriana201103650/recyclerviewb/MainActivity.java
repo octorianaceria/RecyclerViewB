48d68aeca4f4aea352dc3691b2d63f8d4d332b54
juliana <>
2023-06-09 14:41:47 +0700

    a

--- app/src/main/java/com/octoriana201103650/recyclerviewb/MainActivity.java
+++ app/src/main/java/com/octoriana201103650/recyclerviewb/MainActivity.java
@@ -4,9 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.loopj.android.http.AsyncHttpClient;
@@ -18,6 +21,7 @@ import cz.msebera.android.httpclient.Header;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FloatingActionButton _addButton;
     private RecyclerView _recyclerView1;
 
     @Override
@@ -27,30 +31,40 @@ public class MainActivity extends AppCompatActivity {
 
         _recyclerView1 = (RecyclerView) findViewById(R.id.recyclerView1);
 
+
+        initAddButton();
+        loadRecyclerView();
+    }
+
+    private void loadRecyclerView() {
         AsyncHttpClient ahc = new AsyncHttpClient();
         String url = "https://stmikpontianak.net/011100862/tampilmahasiswa.php";
 
-        ahc.get(url, new AsyncHttpResponseHandler()
-        {
+        ahc.get(url, new AsyncHttpResponseHandler() {
             @Override
-            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody)
-            {
-                Gson g = new Gson();
-                List<MahasiswaModel> mahasiswaModelList = g.fromJson(new String(responseBody), new TypeToken<List<MahasiswaModel>>(){}.getType());
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
 
-                RecyclerView.LayoutManager lm = new LinearLayoutManager(MainActivity.this);
-                _recyclerView1.setLayoutManager(lm);
-
-                MahasiswaAdapter ma = new MahasiswaAdapter(mahasiswaModelList);
-                _recyclerView1.setAdapter(ma);
             }
 
             @Override
-            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error)
-            {
-                Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG).show();
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+
             }
         });
+    }
 
+    private void initAddButton() {
+        _addButton = findViewById(R.id.addButton);
+
+        _addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), AddMahasiswaActivity.class);
+                startActivity(intent);
+
+                loadRecyclerView();
+            }
+        });
     }
 }
+
