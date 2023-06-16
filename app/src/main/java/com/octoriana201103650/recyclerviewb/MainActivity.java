4d5459900df5a36ecf691905a967d90ca8b1773d
feniaster <ceriap860@gmail.com>
2023-06-16 13:13:57 +0700

    a

--- app/src/main/java/com/octoriana201103650/recyclerviewb/MainActivity.java
+++ app/src/main/java/com/octoriana201103650/recyclerviewb/MainActivity.java
@@ -43,11 +43,21 @@ public class MainActivity extends AppCompatActivity {
         ahc.get(url, new AsyncHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                Gson g = new Gson();
+
+                List<MahasiswaModel> mahasiswaModelList = g.fromJson(new String(responseBody), new TypeToken<List<MahasiswaModel>>(){}.getType());
+
+                RecyclerView.LayoutManager lm = new LinearLayoutManager(MainActivity.this);
+                _recyclerView1.setLayoutManager(lm);
+
+                MahasiswaAdapter ma = new MahasiswaAdapter(mahasiswaModelList);
+                _recyclerView1.setAdapter(ma);
 
             }
 
             @Override
             public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG).show();
 
             }
         });
